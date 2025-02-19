b17f4b2be4de32b8bec54cdaedf7277f9abe64ea
galsri03 <162865663+galsri03@users.noreply.github.com>
2025-02-19 20:23:59 +0000

    224YayySri

--- Lab24d.java
+++ Lab24d.java
@@ -13,14 +13,21 @@ public class Lab24d
 	public static void main( String args[] ) throws IOException
 	{
 		Scanner file = new Scanner (new File("lab24d.dat"));
-
-
-
-
-
-
-
-
+		int gameNums = file.nextInt();
+		file.nextLine();
+		
+		//Read how many times you should read
+		for (int i=0; i<gameNums; i++) {
+		//TicTacToe only wants this: file.nextLine()
+		String game = file.nextLine();
+		//Make TicTacToe Object 
+		TicTacToe ttt = new TicTacToe(game);
+		//Print the game (TicTacToe)
+		System.out.print("\n" + ttt);
+		//Print the winner
+		System.out.println(ttt.getWinner());
+		}
+		file.close();
 	}
 }
 
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,11 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
 //Lab  -
 
-import java.util.Scanner;
-import static java.lang.System.*;
+//import java.util.Scanner;
+//import static java.lang.System.*;
 
 public class TicTacToe
 {
@@ -13,44 +13,78 @@ public class TicTacToe
 
 	public TicTacToe()
 	{
-
-
+		mat = new char[3][3];
 	}
 
 	public TicTacToe(String game)
 	{
-
-
-
-
+		mat = new char[3][3];
+		int pos = 0;
+		//Row-major order will happen here
+		for (int row=0; row<3;row++) {
+			for (int col=0;col<3; col++) {
+				mat[row][col] = game.charAt(pos);
+				pos++;
+			}
+		}
 	}
 
 	public String getWinner()
 	{
+		//check rows
+		for (int row=0; row<3; row++) {
+			if(mat[row][0]==mat[row][1] && mat[row][1]==mat[row][2]) 
+				{
+					if (mat[row][0]=='X') 
+						return "'X' wins horizontally!";
+					if (mat[row][0]== 'O') 
+						return "'O' wins horizontally!";
 
+				}
+			}
+		
+		//check columns
+		for (int col=0; col<3; col++) {
+			if(mat[0][col]==mat[1][col] && mat[1][col]==mat[2][col]) 
+				{
+					if (mat[0][col]=='X') 
+						return "'X' wins vertically!";
+					if (mat[0][col]== 'O') 
+						return "'O' wins vertically!";
+				}
+			}
+		
+		//Check diagonals
+		if(mat[0][0]==mat[1][1] && mat[1][1]==mat[2][2]) {
+			if (mat[0][0]=='X') 
+				return "'X' wins diagonally!";
+			if (mat[0][0]=='O') 
+				return "'O' wins diagonally!";
+		}
 
+		if(mat[0][2]==mat[1][1] && mat[1][1]==mat[2][0]) {
+			if (mat[0][2]=='X') 
+				return "'X' wins diagonally!";
+			if (mat[0][2]=='O') 
+				return "'O' wins diagonally!";
+		}
 
-
-
-
-
-
-
-
-
-
-		return "";
-	}
+		//return "";
+		return "Scaredy Cat - no winner!";
+	}	
+	
 
 	public String toString()
 	{
-		String output="";
-
-
-
-
-
-
-		return output+"\n\n";
+		String output = "";
+		for (int row=0; row<3;row++) {
+			for (int col=0;col<3; col++) {
+				output += mat[row][col]  + " ";	
+			}
+			//trim output
+			output = output.trim() + "\n";
+		}
+				return output; //+ "\n\n";
 	}
+
 }
